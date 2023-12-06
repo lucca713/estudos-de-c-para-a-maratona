4777003b9373f1207ef8b521b6e9fbe0e06e6f34
lucca713 <luccacerasomma03@gmail.com>
2023-12-06 20:12:07 -0300

    Dei continuidade no jogo de adivinhacao

--- adivinhacao.c
+++ adivinhacao.c
@@ -1,28 +1,48 @@
 #include <stdio.h>
-
+#define NUMERO_DE_TENTATIVAS 5
 
 int main(){
 //minhas variaveis
 int numeroSecreto = 42;
 int chute;
+
     //imprimindo o cabecalho do jogo
     printf("***************************************\n");
     printf("*Bem vindo ao nosso jogo de adivinhacao*\n");
     printf("**************************************\n");
-    
-    printf("qual é seu chute?\n ");
-    scanf("%d", &chute);
 
-    printf("o numero escolhido para o chute foi: %d", chute);
-    if (chute == numeroSecreto)
+    // 
+    for (int i = 1; i <= NUMERO_DE_TENTATIVAS; i++)
     {
-        printf("acertou o numero secreto");
-    }
-    else{
-
-    printf("tento novamente");
-
-    }
     
+       printf("\n Tentativa %d de %d \n", i, NUMERO_DE_TENTATIVAS);
+       printf("qual e seu chute?\n ");
+       scanf("%d", &chute);
+
+        printf("o numero escolhido para o chute foi: %d\n\n", chute);
+
+            int acertou = (chute == numeroSecreto);
+
+            if (acertou)
+            {
+                printf("acertou o numero secreto");
+                // fazer o for parar de funcionar 
+                break; 
+            }
+            else if (chute > numeroSecreto)
+            {
+              printf("Seu chute foi maior do que o numero secreto\n\n  Tente novamente!"); 
+            }
+            else
+            {
+            
+                printf("Seu chute foi menor do que o numero secreto\n\n Tente novamente!");
+
+            }
+
+
+    }
+
+    printf("\n O jogo acabou!");
     
 }
